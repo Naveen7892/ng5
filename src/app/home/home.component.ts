05f0f542869ca3bf60fb14d7575abf7c1a3d434c
Naveen Kumar <metanaveen@outlook.com>
2018-03-12 18:56:09 +0530

    Interpolation, Property & Event binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount = 4;
+  // btnText: string = 'Add an Item';
+  // [tslint] Type string trivially inferred from a string literal, remove type annotation (no-inferrable-types)
+  btnText = 'Add an Item';
+  goalText = 'Goal 1';
+  goals = [];
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+
   }
 
 }
